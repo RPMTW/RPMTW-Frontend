61913358d022b6df7d8a06e9fd82e1415a5010c9
猴子 <a102009102009@gmail.com>
2022-07-14 07:04:13 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+// import { createApp } from 'vue';
+import { ViteSSG } from 'vite-ssg';
+import 'virtual:svg-icons-register';
 
-createApp(App).mount('#app');
+import router from './router';
+
+import '@/scss/main.scss';
+import store from './stores';
+
+import App from '@/layouts/default.vue';
+import SvgIconComponent from '@/components/utils/SvgIcon.vue';
+
+export const createApp = ViteSSG(
+  App,
+  router,
+  ({ app }) => {
+    app.use(store);
+    app.component('SvgIcon', SvgIconComponent);
+  },
+  { rootContainer: '#app-mount' }
+);
